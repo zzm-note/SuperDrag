3387a72caaf9c5da43b2bad4d2d5f8620cf5f3ff
zzm-note <zzm.note@foxmail.com>
2020-04-16 01:28:22 +0800

    day1_3

--- common.js
+++ common.js
@@ -7,56 +7,56 @@ function _getDefault() {
 
 var _build_in_seach_engines = [{
 			"name" : "wikipedia",
-			"url" : "http://en.wikipedia.org/wiki/How%s"
+			"url" : "https://en.wikipedia.org/wiki/How%s"
 		}, {
 			"name" : "google",
-			"url" : "http://www.google.com/search?hl=en&q=%s"
+			"url" : "https://www.google.com/search?hl=en&q=%s"
 		}, {
 			"name" : "douban",
-			"url" : "http://www.douban.com/search?search_text=%s"
+			"url" : "https://www.douban.com/search?search_text=%s"
 		}, {
 			"name" : "twitter",
-			"url" : "http://twitter.com/search?q=%s"
+			"url" : "https://twitter.com/search?q=%s"
 		}, {
 			"name" : "bing",
-			"url" : "http://www.bing.com/search?setmkt=en-US&q=%s"
+			"url" : "https://www.bing.com/search?setmkt=en-US&q=%s"
 		}, {
 			"name" : "taobao",
-			"url" : "http://s.taobao.com/search?q=%s"
+			"url" : "https://s.taobao.com/search?q=%s"
 		}, {
 			"name" : "shooter",
-			"url" : "http://shooter.cn/search/%s"
+			"url" : "https://shooter.cn/search/%s"
 		}, {
 			"name" : "amazon",
-			"url" : "http://www.amazon.com/s/field-keywords=%s"
+			"url" : "https://www.amazon.com/s/field-keywords=%s"
 		}, {
 			"name" : "delicious",
-			"url" : "http://delicious.com/search?p=%s"
+			"url" : "https://delicious.com/search?p=%s"
 		}, {
 			"name" : "flickr",
-			"url" : "http://www.flickr.com/search/?q=%s"
+			"url" : "https://www.flickr.com/search/?q=%s"
 		}, {
 			"name" : "technorati",
-			"url" : "http://technorati.com/search?return=posts&authority=high&q=%s"
+			"url" : "https://technorati.com/search?return=posts&authority=high&q=%s"
 		}, {
 			"name" : "youtube",
-			"url" : "http://www.youtube.com/results?search_query=%s"
+			"url" : "https://www.youtube.com/results?search_query=%s"
 		}, {
 			"name" : "ebay",
-			"url" : "http://shop.ebay.com/?_nkw=%s&_sacat=See-All-Categories"
+			"url" : "https://shop.ebay.com/?_nkw=%s&_sacat=See-All-Categories"
 		}, {
 			"name" : "imdb",
-			"url" : "http://www.imdb.com/find?s=all&q=%s"
+			"url" : "https://www.imdb.com/find?s=all&q=%s"
 		}, {
 			"name" : "google encrypted",
 			"url" : "https://encrypted.google.com/search?hl=en&q=%s"
 		}, {
 			"name" : "wolfram alpha",
-			"url" : "http://www.wolframalpha.com/input/?i=%s"
+			"url" : "https://www.wolframalpha.com/input/?i=%s"
 		}, {
 			"name" : "yahoo",
-			"url" : "http://search.yahoo.com/search?fr=crmas&p=%s"
+			"url" : "https://search.yahoo.com/search?fr=crmas&p=%s"
 		}, {
 			"name" : "pinterest",
-			"url" : "http://pinterest.com/search/?q=%s"
+			"url" : "https://pinterest.com/search/?q=%s"
 		}];
